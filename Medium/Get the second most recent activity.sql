673e853ab15f1f34738d4f4f9c2c3ac9a0169e07
vinothebusybee <51785775+vinothebusybee@users.noreply.github.com>
2022-02-09 14:27:51 +0530

    Update Get the second most recent activity.sql

--- Medium/Get the second most recent activity.sql
+++ Medium/Get the second most recent activity.sql
@@ -50,3 +50,28 @@ rank() over(partition by username order by startdate desc) as rk,
 count(username) over(partition by username) as cnt
 from useractivity) a
 where a.rk = 2 or cnt = 1
+
+
+%python
+data = [
+        ["Alice", "Travel", "2020-02-12", "2020-02-20"], 
+        ["Alice", "Dancing", "2020-02-21", "2020-02-23"], 
+        ["Alice", "Travel", "2020-02-24", "2020-02-28"], 
+        ["Bob", "Travel", "2020-02-11", "2020-02-18"], 
+       ]
+		
+columns = ["username", "activity", "startDate", "endDate"]
+
+df = spark.createDataFrame(data, columns)
+
+df.createOrReplaceTempView("tbl")
+
+
+%sql
+select username,activity,startDate,endDate from (
+select * ,
+count(username) over(partition by username) cnt,
+dense_rank() over(partition by username order by startDate desc) rnk
+from tbl)
+where rnk = 2 or cnt = 1
+
